cdd1908a9ceaf61cd30b1912ef9b0ec3fa7fb493
Ikran Askar <109754188+ikranaskar@users.noreply.github.com>
2023-03-18 18:08:47 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,79 +1,66 @@
-f#rom msilib.schema import File
 import os
 import csv
-csvpath = os.path.join("..", "Resources", "election_data.csv")
-print (election_data.csv)
+#csvpath = os.path.join('/Users/ikran/OneDrive/Documents/Desktop/Python_challenge/PyBank/Resources/budget_data.csv')
+cvspath = (f"../Resources/budget_data.csv")
+
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
+#print (budget_data.csv)
+print(f"current dir: {os.getcwd()}")
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
     print(csvreader)
 
-    total_votes = 0 
-    Charles_votes = 0
-    Diana_votes = 0
-    Raymon_votes = 0
-    candidates = 0
-    next(csvreader)
-    data = list(csvreader)
-    row_count = len(data)
-    unique_name1 = []
-# Each row is read as a row
+    header = next(csvreader) # starts data on the next row
+    total_month_count = 0 # store the count of all rows which provides month count
+    budget_1 = [] # store budget values in this list
+    total_budget = 0 # used to get the total sum of budget
+    month_1 = [] # store month in this list
+
     for row in csvreader:
-        #print(row)
-
-        #A complete list of candidates who received votes
-
-
-        #the total number of votes cast
-        total_votes +=1
-
-        #The percentage of votes each candidate won
-        if row[2] == "Charles":
-            Charles_votes +=1
-        elif row[2] == "Diana":
-            Diana_votes +=1
-        elif row[2] == "Raymon":
-            Raymon_votes +=1
+        total_month_count+=1
+        budget_1.append(row[1])
+        month_1.append(row[0])
 
 
-candidates = ["Charles", "Diana", "Raymon"]
-votes = [Charles_votes, Diana_votes, Raymon_votes]
+    for x in budget_1:
+         total_budget+=  float(x) # sums all data and gets the total
+            
+    budget_t = round(total_budget,) # store the total budget to output into text file
+    profit_change = round((float(budget_1[len(budget_1) -1]) - float(budget_1[0]))/float(len(budget_1)-1),2) # used to get the average change
+            
 
-dict_candidatesvotes = dict(zip(candidates,votes))
-key = max(dict(dict_candidatesvotes, key=dict_candidatesvotes.get))
-candilist = list()
-tally = list()
-for i in range (0,row_count):
-    candidate = data[i][2]
-    tally.append(candidate)
-if candidate not in candilist: 
-    candilist.append(candidate)
-    candicount = len(candilist)
+    first_number = 0 # looks the first number
+    second_number = 0 # looks at second number
+    profits_review = [] # used to append data to review max and min
+    for i in budget_1:
+        second_number = second_number + 1 # get the next second number
+        if(second_number < len(budget_1)): # second number is less than the total number
+            first_1 = budget_1[first_number] # assign first number 
+            second_1 = budget_1[second_number] # assign second number
+            profits_sub= float(second_1)-float(first_1) # second num - first num to get the gains and losses
+            profits_review.append(profits_sub) # put the gains and losses in a list
+            first_number = first_number + 1 # got to the next first number
+        else:
+            0
 
-  # The total number of votes each candidate won & the percentage of votes each candidate won
-    votes = list()
-    percentage = list()
-    for j in range (0,candicount):
-        name = candilist[j]
-        votes.append(tally.count(name))
-        vprct = votes[j]/row_count
-        percentage.append(vprct)
+    max_number = round(max(profits_review),) # get the max number in the list of gains and losses   
+    min_number = round(min(profits_review),) # get the min number in the list of gains and losses
+    month_min = profits_review.index(min_number) +1 # find the index of the biggest loss within the list of gains and losses
+    month_max = profits_review.index(max_number) +1 # find the index of the highest gains within the list of gains and losses
+    m1 = month_1[month_max] # get the month that had the highest gains
+    m2 = month_1[month_min] #get the month that had the biggest loss
 
-  # The winner of the election based on popular vote.
-    winner = votes.index(max(votes))    
-
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-  # Print the results to terminal
-    print("Election Results")
-    print("----------------------------")
-    print(f"Total Votes: {row_count:,}")
-    print("----------------------------")
-    for k in range (0,candicount): 
-        print(f"{candilist[k]}: {percentage[k]:.3%} ({votes[k]:,})")
-    print("----------------------------")
-    print(f"Winner: {candilist[winner]}")
-    print("----------------------------")
+print("Financial Analysis")
+print('--------------------')
+print(f"Total Months: {total}")
+print(f"Total: ${round(count)}")
+print(f"Average Change: {round(sum(monthly_profit)/len(monthly_profit),2)}")
+print(f"Greatest Increase in Profits: (${max_increase_value})")
+print(f"Greatest Decrease in Profits: (${min_increase_value})")
 
 output_file = Path ("..", "Resources", "Analysis.txt")
-with open(output_file, "w") as File
+with open(output_file, "w") as File:
+# filepath = os.path.join('.', 'Resources', 'Analysis.txt')
+# with open(filepath, "w") as text_file
